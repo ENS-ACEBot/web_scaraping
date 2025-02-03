73da7bef3428e958aa28675780a3fef19c3d53c7
burakmert824 <burakmert824@gmail.com>
2025-02-04 00:33:49 +0300

    write scrape_time_interval function, fix comments, add convert_kapnews_to_news add filter_kap_news_and_extract_content add save_news_to_json add bist 50 stock codes

--- kap_scraping.py
+++ kap_scraping.py
@@ -8,34 +8,44 @@ import os
 from scrapers.abstract_news_scraper import AbstractNewsScraper
 import time
 import html
+import bist_50
+from common.new_class import News
 
 
 DISCLOSURE_INTERVAL_ENDPOINT = "https://www.kap.org.tr/tr/api/memberDisclosureQuery"
-
+# usage = post request 
+# body : { toDate: "%Y-%m-%d", fromDate: "%Y-%m-%d" }
 DISCLOSURE_BY_INDEX_ENDPOINT = "https://www.kap.org.tr/tr/Bildirim/"
 # "https://www.kap.org.tr/tr/Bildirim/<index>"
 class KapNewsScraper(AbstractNewsScraper):
     """
     Scraper for fetching KAP disclosures.
     """
-    disclosure_date_format = "%Y-%m-%d"
+    def __init(self):
+        
+        pass
+    #disclosure_date_format = "%Y-%m-%d"
     
-    def scrape_time_interval(self, start_date: str, end_date: str):
+    def scrape_time_interval(self, start_date: str, end_date: str) -> list[News]:
         """
-        Scrapes KAP disclosures within the given date range.
+        Scrape news for the given time interval and return a list of News objects.
+        date format : "YYYY-MM-DD"
         """
-        # start = datetime.strptime(start_date, self.disclosure_date_format)
-        # end = datetime.strptime(end_date, self.disclosure_date_format)
-
-        # # Generate API query parameters (assuming KAP API supports filtering by date)
-        # url = self.base_url.format(start_date=start.strftime("%Y-%m-%d"), end_date=end.strftime("%Y-%m-%d"))
-        # logging.info(f"Fetching data from {url}")
-
-        # json_data = self.fetch_json(url)
-        # if not json_data:
-        #     return []
-
-        # return self.extract_news_data(json_data)
+        try:
+            logging.info(f"Scraping news for time interval started : ")
+            logging.info(f"Scraping KAP disclosures from {start_date} to {end_date}")
+            fetched_disclosures = self.get_all_kap_disclosures_for_given_range(start_date, end_date)
+            logging.info(f"Scraping for news for time interval finished : {len(fetched_disclosures)} disclosures fetched")
+            logging.info(f"Filtering and extracting content for BIST 50 companies for proper type disclosures")
+            kapNews = self.filter_kap_news_and_extract_content(fetched_disclosures, bist_50.BIST_50_SYMBOLS)
+            logging.info(f"Filtered and extracted content for BIST 50 companies : {len(kapNews)} disclosures")
+            logging.info(f"Converting KapNews to News objects")
+            news_list = self.convert_kapnews_to_news(kapNews)
+            logging.info(f"Converted KapNews to News objects : {len(news_list)} news")
+            return news_list
+        except Exception as e:
+            logging.error(f"Error occured while scraping news for time interval started : {e}")
+            return []
     
     def save_disclosures_json(self, kap_disclosures: list[KapNews], from_date: str, to_date: str, directory: str):
         """
@@ -59,6 +69,7 @@ class KapNewsScraper(AbstractNewsScraper):
             json.dump([disclosure.to_dict() for disclosure in kap_disclosures], file, ensure_ascii=False, indent=4)
         logging.info(f"KAP disclosures saved to {file_path}")
     
+    # fetch all kap disclosures in the given range and return the list of KapNews objects without content
     def get_all_kap_disclosures_for_given_range(self, from_date: str, to_date: str):
         """_summary_
             it uses fetch_kap_disclosures method to get all kap disclosures with intervals
@@ -98,7 +109,7 @@ class KapNewsScraper(AbstractNewsScraper):
         logging.info("-" * 50)  
         return all_kap_disclosures
 
-
+    # fetch the kap disclosures from the given date range and return the list of KapNews objects without content
     def fetch_kap_disclosures(self, from_date: str, to_date: str) -> list[KapNews]:
         """
         Sends a POST request to the KAP API to retrieve disclosures within the given date range.
@@ -143,8 +154,9 @@ class KapNewsScraper(AbstractNewsScraper):
 
         logging.error("All attempts to fetch KAP disclosures failed.")
         return []
-    ###### main function to fetch disclosures in the given interval and save
-    # it is used to fetch and save the disclosures in the given interval
+   
+    ###### main function to fetch disclosures without content in the given interval and save disclosures in the json files
+    # it is used to fetch and save the disclosures in the given interval 
     def fetch_kap_disclosures_interval_year(self,start_year: int, end_year: int):
         '''
             divide kap disclosures into years and fetch them
@@ -156,7 +168,7 @@ class KapNewsScraper(AbstractNewsScraper):
             scraper.save_disclosures_json(kap_disclosures, from_date, to_date, "kap/data")
     
     # it is used to retrive the kap disclosures from the json files
-    def load_kap_news_from_json_files(self,start_year: int, end_year: int, directory: str) -> list[KapNews]:
+    def load_kap_news_from_json_files(self,directory: str) -> list[KapNews]:
         """
         Load KapNews objects from JSON files in the specified directory, starting from the given year and decreasing until the end year.
 
@@ -170,20 +182,20 @@ class KapNewsScraper(AbstractNewsScraper):
         """
         kap_news_list = []
 
-        for year in range(start_year, end_year, 1):
-            file_path = os.path.join(directory, f"kap_disclosures_{year}-01-01_{year}-12-31.json")
-            if os.path.exists(file_path):
-                with open(file_path, 'r', encoding='utf-8') as file:
-                    data = json.load(file)
-                    for item in data:
-                        kap_news_list.append(KapNews.from_dict(item))
-            else:
-                print(f"File not found: {file_path}")
+        file_path = directory
+        if os.path.exists(file_path):
+            with open(file_path, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                for item in data:
+                    kap_news_list.append(KapNews.from_dict(item))
+        else:
+            print(f"File not found: {file_path}")
+            
         return kap_news_list
     
-    # disclosureCategory    : ODA
-    # disclosureClass       : ODA
-    def scrape_disclosure_by_index_ODA_ODA(self,index: int) -> dict:
+    # scrape the given indexed disclousre's content from the kap website and return the content
+    # condition : disclosureClass = ODA, disclosureCategory = ODA
+    def scrape_disclosure_by_index_ODA_ODA(self,index: int) -> str:
         """
         Scrape the disclosure details from the endpoint using the given index.
 
@@ -232,9 +244,9 @@ class KapNewsScraper(AbstractNewsScraper):
         print(f"Failed to fetch disclosure by index {index}: {e}")
         return ""
     
-    # disclosureCategory    : STT
-    # disclosureClass       : ODA
-    def scrape_disclosure_by_index_ODA_STT(self,index: int) -> dict:
+    # scrape the given indexed disclousre's content from the kap website and return content
+    # condition : disclosureCategory = STT, disclosureClass = ODA
+    def scrape_disclosure_by_index_ODA_STT(self,index: int) -> str:
         """
         Scrape the disclosure details from the endpoint using the given index.
 
@@ -294,45 +306,103 @@ class KapNewsScraper(AbstractNewsScraper):
 
         print(f"Failed to fetch disclosure by index {index}: {e}")
         return ""
+    
+    def convert_kapnews_to_news(self,kap_news_list: list[KapNews]) -> list[News]:
+        """
+        Convert a list of KapNews objects to a list of News objects.
+
+        Parameters:
+        kap_news_list (List[KapNews]): A list of KapNews objects.
+
+        Returns:
+        List[News]: A list of News objects.
+        """
+        news_list = []
+        for kap_news in kap_news_list:
+            title = f"{kap_news.disclosure_index} - {kap_news.stock_codes} - {kap_news.kap_title}"
+            news = News(
+                title=title,
+                content=kap_news.content,
+                date_time=kap_news.publish_date,
+                source="KAP", 
+                news_url=f"https://www.kap.org.tr/tr/Bildirim/{kap_news.disclosure_index}" 
+            )
+            news_list.append(news)
+        return news_list
+    
+    def filter_kap_news_and_extract_content(kap_news_list: list[KapNews], stock_codes: list[str]) -> list[KapNews]:
+        '''
+            filter the kap news list by stock codes and disclosure type and extract the content
+        '''
+        nw_disclosures = []
+        print(len(disclosures))
+        for disclosure in disclosures:
+            if(disclosure.stock_codes in bist_50.BIST_50_SYMBOLS):
+                print(f"found : {disclosure.stock_codes} - {disclosure.disclosure_index}")
+                if disclosure.disclosure_class == "ODA" :
+                    if disclosure.disclosure_category == "STT":
+                        content = scraper.scrape_disclosure_by_index_ODA_STT(disclosure.disclosure_index)
+                        if content == "": 
+                            logging.info(f"Failed to fetch content for index {disclosure.disclosure_index}")
+                            continue
+                        disclosure.content = content
+                        nw_disclosures.append(disclosure)
+                        
+                    elif disclosure.disclosure_category == "ODA" or disclosure.disclosure_type == "ODA":
+                        content = scraper.scrape_disclosure_by_index_ODA_ODA(disclosure.disclosure_index)
+                        if content == "": 
+                            logging.info(f"Failed to fetch content for index {disclosure.disclosure_index}")
+                            continue
+                        disclosure.content = content
+                        nw_disclosures.append(disclosure)
+                    time.sleep(0.01)
+            logging.info(f"Processed {cnt} disclosures : remaining {len(disclosures) - cnt}")
+            cnt+=1
+        return nw_disclosures
+    
+    def save_news_to_json(self,news_list, db_file):
+        """
+        Save a list of News objects to a JSON database file.
+        
+        Parameters:
+        news_list (list): A list of News objects.
+        db_file (str): The name of the JSON database file.
+        """
+        # Append new news to the existing data
+        news_data = [news.to_dict() for news in news_list]
+
+        # Save the updated data back to the database file
+        with open(db_file, 'w') as file:
+            json.dump(news_data, file, ensure_ascii=False, indent=4)
+        
+        return news_data
         
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     scraper = KapNewsScraper()
-    
-    #get old scraped disclosures data
-    for i in range(2024,2025):
+    # 2017 den once old kap oluyormus orda farkli bir data scraping lazim
+    # get old scraped disclosures data
+    # for i in range(2017,2024):
+    #     cnt = 0
+    #     from_date   = f"{i}-01-01"
+    #     to_date     = f"{i}-12-31"         
+    #     disclosures = scraper.load_kap_news_from_json_files(i,i+1, "kap/data")
+    #     print(f"Loaded {len(disclosures)} disclosures - {from_date} to {to_date}")
+    #     nw_disclosures = scraper.filter_kap_news_and_extract_content(disclosures, bist_50.BIST_50_SYMBOLS)
+    #     scraper.save_disclosures_json(nw_disclosures, from_date, to_date, "kap/data_with_content_bist50")
+    #     print(f"Saved {len(nw_disclosures)} disclosures - {from_date} to {to_date} ")
+    for i in range(2017,2025):
         cnt = 0
         from_date   = f"{i}-01-01"
-        to_date     = f"{i}-12-31"         
-        disclosures = scraper.load_kap_news_from_json_files(i,i+1, "kap/data")
-        print(f"Loaded {len(disclosures)} disclosures - {from_date} to {to_date}")
-        nw_disclosures = []
-        print(len(disclosures))
-        for disclosure in disclosures:
-            if disclosure.disclosure_class == "ODA" :
-                if disclosure.disclosure_category == "STT":
-                    content = scraper.scrape_disclosure_by_index_ODA_STT(disclosure.disclosure_index)
-                    if content == "": 
-                        logging.info(f"Failed to fetch content for index {disclosure.disclosure_index}")
-                        continue
-                    disclosure.content = content
-                    nw_disclosures.append(disclosure)
-                    
-                    
-                if disclosure.disclosure_category == "ODA":
-                    content = scraper.scrape_disclosure_by_index_ODA_ODA(disclosure.disclosure_index)
-                    if content == "": 
-                        logging.info(f"Failed to fetch content for index {disclosure.disclosure_index}")
-                        continue
-                    disclosure.content = content
-                    nw_disclosures.append(disclosure)
-                time.sleep(0.01)
-            logging.info(f"Processed {cnt} disclosures : remaining {len(disclosures) - cnt}")
-            cnt+=1
-        scraper.save_disclosures_json(nw_disclosures, from_date, to_date, "kap/data_with_content")
-        print(f"Saved {len(nw_disclosures)} disclosures - {from_date} to {to_date} ")
-
-                               
+        to_date     = f"{i}-12-31"    
+        file_name = f"kap/data_with_content_bist50/kap_disclosures_{from_date}_{to_date}.json"
+        disclosures =  scraper.load_kap_news_from_json_files(file_name)
+        news_list = scraper.convert_kapnews_to_news(disclosures)
+        print(f"Converted {len(news_list)} disclosures to news")
+        db_file = f"kap/data_with_content_bist50_general_class/news_{from_date}_{to_date}.json"
+        scraper.save_news_to_json(news_list,db_file)
+    
+                       
     # try to fetch single disclosure's aciklamalar ODA ODA
     # index = 988819
     # print(scraper.scrape_disclosure_by_index_ODA_ODA(index))
