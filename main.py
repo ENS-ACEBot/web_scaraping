5ef0c1082b7f412a9f3776a376fba920cdfcf0d2
burakmert824 <burakmert824@gmail.com>
2025-02-05 04:17:22 +0300

    add main function to run scrapers

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from scrapers.abstract_news_scraper import AbstractNewsScraper
 from scrapers.mynet_scraper import MynetNewsScraper 
 from scrapers.kap_scraper import KapNewsScraper
+from scrapers.bigpara_scraper import BigparaNewsScraper
 import logging
 import datetime
 from common.new_class import News
+
 
 
 
@@ -20,7 +22,15 @@ if __name__ == "__main__":
     mynet_news = mynet_scraper.scrape_time_interval(fetching_date_str,fetching_date_str)
 
     kap_scraper = KapNewsScraper()
+    
     kap_news = kap_scraper.scrape_time_interval(fetching_date_str,fetching_date_str)
-    for news in kap_news:
-        print(news.title)
-        print(news.date_time)
+    
+    bigpara_scraper = BigparaNewsScraper()
+    
+    bigpara_news =  bigpara_scraper.scrape_time_interval(fetching_date_str,fetching_date_str)
+    
+    # for news in bigpara_news:
+    #     print(news.title)
+    #     print(news.date_time)
+
+    
